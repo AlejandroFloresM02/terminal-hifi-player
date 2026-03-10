e36f9357d71a2bfae5c92cd28526cc3ff63e2e27
AlejandroFloresM02 <madrizfa02@gmail.com>
2026-03-10 16:14:29 -0600

    Initiall work, created supported files ENUM and initialized based python files

---
+++ flac_player/file_finder.py
@@ -0,0 +1,28 @@
+import argparse
+import os
+from supported_file_types import SupportedFiles
+import numpy as np
+
+
+def find_audio_file(directory: str):
+    queue_array = np.array([])
+    files = os.listdir(directory)
+    for file in files:
+        if file.endswith(SupportedFiles.FLAC.value) or file.endswith(
+            SupportedFiles.MP3.value
+        ):
+            queue_array = np.append(queue_array, os.path.join(directory, file))
+            print(f"[DEBUG] {queue_array}")
+            return queue_array
+    return False
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("directory", help="Directory to search for audio files")
+    args = parser.parse_args()
+
+    if find_audio_file(args.directory):
+        print("[DEBUG] Audio file found!")
+    else:
+        print("[DEBUG] No audio file found.")
---
+++ flac_player/supported_file_types.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class SupportedFiles(Enum):
+    FLAC = ".flac"
+    MP3 = ".mp3"
